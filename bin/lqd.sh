4293616bb90ab6479f9368848c56825bba644a59
Daniel Carr <d.douglas.carr@gmail.com>
2016-04-16 11:45:30 +1000

    bin/lqd context changed to ./, bin/sql mv to ./

--- bin/lqd.sh
+++ bin/lqd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Can be called from anywhere, will run from this dir.
+# Can be called from anywhere, will run from base dir of repo.
 WORKDIR=`dirname "$(readlink -f "$0")"`
 cd $WORKDIR
+cd ../
 
 HELP_TEXT="
 Core utility commands
@@ -12,22 +13,26 @@ Usage:
 
 Commands:
   db              Database commands (core.sh db.help for information)
+  system          Manage containers
 
 "
 
 while [ "$1" != "" ]; do
   case $1 in 
 
-    "db" )      shift
-                exec ./subcommands/db.sh $@
-                exit
-                ;;
+    "db" )                      shift
+                                exec ./bin/subcommands/db.sh $@
+                                exit
+                                ;;
+    "system" | "sys" )          shift
+                                exec ./bin/subcommands/system.sh $@
+                                ;;
     help | --help | -h )        echo "$HELP_TEXT"
-                exit 0;
-                ;;
-    * )         echo "command not found see --help"
-                exit 1;
-                ;;
+                                exit 0;
+                                ;;
+    * )                         echo "command not found see --help"
+                                exit 1;
+                                ;;
   esac
   shift
 done
